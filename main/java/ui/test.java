146419c66655db00e6fa9de574513784f499bab7
ceciliachalar <ceci.chiqui@gmail.com>
2015-11-10 18:44:19 -0400

    Adding the skeleton

---
+++ main/java/ui/test.java
@@ -0,0 +1,11 @@
+package ui;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ceciliachalar
+ * Date: 11/10/15
+ * Time: 6:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}
